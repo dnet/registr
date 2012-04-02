12d05d7b0c403571d2b79c891cb94b1436583316
András Veres-Szentkirályi <vsza@vsza.hu>
2012-04-02 15:45:09 +0200

    shifter: walk in topological order to handle merges

--- shifter.py
+++ shifter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pygit2 import Repository, GIT_SORT_REVERSE
+from pygit2 import Repository, GIT_SORT_REVERSE, GIT_SORT_TOPOLOGICAL
 from time import time
 from os import getpid
 import re
@@ -14,7 +14,7 @@ def shift(amount, repo_path):
     adder = Adder(amount)
     changelog = dict()
     reference = REF_FMT.format(time=time(), pid=getpid())
-    for commit in repo.walk(head.oid, GIT_SORT_REVERSE):
+    for commit in repo.walk(head.oid, GIT_SORT_REVERSE | GIT_SORT_TOPOLOGICAL):
         newmsg, nsubs = ISSUE_RE.subn(adder.add, commit.message)
         if nsubs != 0 or any(pnt.oid in changelog for pnt in commit.parents):
             parents = [changelog.get(c.oid, c.oid) for c in commit.parents]
