905286376c4aae87ed7d28e82d541bbf6ef702e4
András Veres-Szentkirályi <vsza@vsza.hu>
2012-04-02 18:28:09 +0200

    added working Redmine updater

---
+++ redmine.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from sqlalchemy import create_engine, Table, Column, String, Integer, MetaData
+from sqlalchemy.sql import column
+from binascii import unhexlify, hexlify
+import re
+
+TABLES = [
+        ('journals', 'notes'),
+        ('time_entries', 'comments'),
+        ('wiki_content_versions', 'data'),
+        ]
+
+COMMIT_ID_RE = r'(commit:)?[0-9a-f]{7,40}'
+ID_COLUMN = 'id'
+
+def test_db_connection(db_url):
+    db_connect(db_url).close()
+
+def map_changelog(db_url, changelog):
+    with db_connect(db_url) as connection:
+        metadata = MetaData()
+        replacer = Replacer(changelog)
+        for table_name, column_name in TABLES:
+            table = Table(table_name, metadata,
+                    Column(ID_COLUMN, Integer, primary_key=True),
+                    Column(column_name, String(255)))
+            commit_id_clause = column(column_name).match(COMMIT_ID_RE)
+            result = connection.execute(table.select().where(commit_id_clause))
+            to_replace = []
+            for row_id, content in result:
+                replaced = re.sub(COMMIT_ID_RE, replacer.repl, content)
+                if replaced != content:
+                    to_replace.append((row_id, replaced))
+            result.close()
+            id_col = column(ID_COLUMN)
+            for row_id, content in to_replace:
+                update_where = table.update().where(id_col == row_id)
+                connection.execute(update_where.values({column_name: content}))
+
+def db_connect(db_url):
+    return create_engine(db_url).connect()
+
+class Replacer(object):
+    def __init__(self, changelog):
+        self.changelog = changelog
+
+    def repl(self, match):
+        commit_id = match.group(0)
+        sha = commit_id[7:] if commit_id.startswith('commit:') else commit_id
+        oid = unhexlify(sha if len(sha) % 2 == 0 else sha[:-1])
+        if len(sha) == 40:
+            return 'commit:' + self.changelog.get(oid, sha)
+        else:
+            for old, new in self.changelog.iteritems():
+                if old.startswith(oid):
+                    return 'commit:' + hexlify(new)[:7]
+            return commit_id
--- registr.py
+++ registr.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 from shifter import shift
+from redmine import test_db_connection, map_changelog
 
 def main(args):
     try:
@@ -9,14 +10,21 @@ def main(args):
         amount = int(amount)
     except ValueError:
         raise InvalidUsageError(args[0])
+    try:
+        db_url = args[3]
+        test_db_connection(db_url)
+    except IndexError:
+        db_url = None
     changelog, reference = shift(int(amount), repo_path)
     print('HEAD is shifted as', reference)
+    if db_url is not None:
+        map_changelog(db_url, changelog)
 
 class InvalidUsageError(ValueError):
     def __init__(self, exename):
         self.exename = exename
 
-    USAGE_ERROR_FMT = 'Usage: {exe} <amount> <path to repo>'
+    USAGE_ERROR_FMT = 'Usage: {exe} <amount> <path to repo> [database URL]'
     def __str__(self):
         return self.USAGE_ERROR_FMT.format(exe=self.exename)
 
