f8d0db90543637c10e8e914902c500ea1f9d5436
András Veres-Szentkirályi <vsza@vsza.hu>
2012-04-04 11:48:39 +0200

    shifter: replaced Adder class with functools.partial

--- shifter.py
+++ shifter.py
@@ -3,6 +3,7 @@
 from pygit2 import Repository, GIT_SORT_REVERSE, GIT_SORT_TOPOLOGICAL
 from time import time
 from os import getpid
+from functools import partial
 import re
 
 ISSUE_RE = re.compile(r'#([0-9]+)')
@@ -11,11 +12,11 @@ REF_FMT = 'refs/heads/registr-{time}-{pid}'
 def shift(amount, repo_path):
     repo = Repository(repo_path)
     head = repo.lookup_reference('HEAD').resolve()
-    adder = Adder(amount)
+    adder = partial(add, amount=amount)
     changelog = dict()
     reference = REF_FMT.format(time=time(), pid=getpid())
     for commit in repo.walk(head.oid, GIT_SORT_REVERSE | GIT_SORT_TOPOLOGICAL):
-        newmsg, nsubs = ISSUE_RE.subn(adder.add, commit.message)
+        newmsg, nsubs = ISSUE_RE.subn(adder, commit.message)
         if nsubs != 0 or any(pnt.oid in changelog for pnt in commit.parents):
             parents = [changelog.get(c.oid, c.oid) for c in commit.parents]
             new_oid = repo.create_commit(reference, commit.author,
@@ -23,9 +24,5 @@ def shift(amount, repo_path):
             changelog[commit.oid] = new_oid
     return changelog, reference
 
-class Adder(object):
-    def __init__(self, amount):
-        self.amount = amount
-
-    def add(self, match):
-        return '#{0}'.format(int(match.group(1)) + self.amount)
+def add(match, amount):
+    return '#{0}'.format(int(match.group(1)) + amount)
